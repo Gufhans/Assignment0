c48a559fa6da6a109a6b550286e049644d05edbb
Jakob <jakst@itu.dk>
2022-09-05 11:27:44 +0200

    First build achieved

--- assignment0app/Program.cs
+++ assignment0app/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace assignment0;
+
+
+public class Exercise3 
+{
+    
+    public static void Main(string[]args)
+    {
+        
+    }
+    
+    public Exercise3()
+    {
+
+    }
+
+    public bool isLeapYear(int year) => (year%4 == 0 || year%400 == 0) ? true : false;
+}
--- xunitstuff/UnitTest1.cs
+++ xunitstuff/UnitTest1.cs
@@ -1,10 +1,62 @@
-namespace xunitstuff;
+namespace Program.Tests;
+using assignment0;
+using Xunit;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void DivisibleByFourWithTrueInput()
     {
+        //Arrange
+        var is_This_A_Leap_Year = new Exercise3();
 
+
+        //Act
+        var output = is_This_A_Leap_Year.isLeapYear(8);
+
+        //Assert
+        output.Should().Be(true);
+    }
+
+    [Fact]
+    public void DivisibleByFourWithFalseInput()
+    {
+        //Arrange
+        var is_This_A_Leap_Year = new Exercise3();
+
+
+        //Act
+        var output = is_This_A_Leap_Year.isLeapYear(9);
+
+        //Assert  
+        output.Should().Be(false);  
+    }
+
+    [Fact]
+    public void DivisibleBy400WithTrueInput()
+    {
+        //Arrange
+        var is_This_A_Leap_Year = new Exercise3();
+
+
+        //Act
+        var output = is_This_A_Leap_Year.isLeapYear(1600);
+
+        //Assert
+        output.Should().Be(true);
+    }
+
+    [Fact]
+    public void DivisibleBy400WithFalseInput()
+    {
+        //Arrange
+        var is_This_A_Leap_Year = new Exercise3();
+
+
+        //Act
+        var output = is_This_A_Leap_Year.isLeapYear(1700);
+
+        //Assert
+        output.Should().Be(false);
     }
 }
